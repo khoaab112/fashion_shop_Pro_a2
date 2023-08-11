f819c78cd38843055e6e738973c8abe4e2f9e2dd
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2023-08-11 16:44:05 +0700

    fix file app.js

--- fashion_system/resources/js/app.js
+++ fashion_system/resources/js/app.js
@@ -1,20 +1,27 @@
 import { createApp } from 'vue';
+//element-plus
 import ElementPlus from 'element-plus';
 import VN from 'element-plus/dist/locale/vi.mjs';
 // import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css';
+//ant-design
 import { DatePicker, Radio } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-import App from './App.vue';
-import router from './routerVue/index.js';
+//bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+//axios
 import axios from 'axios';
+//font-awesome
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+
+//app
+import App from './App.vue';
+import router from './routerVue/index.js';
 
 window.axios = axios;
 
