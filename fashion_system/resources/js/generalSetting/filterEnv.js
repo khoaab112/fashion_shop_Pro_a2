bf95b86994ccf01a55cfe3c32bde997f3b77a8d6
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2023-08-11 16:06:23 +0700

    update

--- fashion_system/resources/js/generalSetting/filterEnv.js
+++ fashion_system/resources/js/generalSetting/filterEnv.js
@@ -3,6 +3,7 @@ const ENV = {
     KEY_ACCESS_TOKEN: import.meta.env.VITE_KEY_ACCESS_TOKEN,
     KEY_REFRESH_TOKEN: import.meta.env.VITE_KEY_REFRESH_TOKEN,
     TIME_REFRESH_TOKEN: import.meta.env.VITE_TIME_REFRESH_TOKEN_CLIENT,
+    VERSION: import.meta.env.VITE_VERSION
 };
 
 export default ENV;
--- fashion_system/resources/js/viewsVue/admin/menuStaff/supportForEmployees.vue
+++ fashion_system/resources/js/viewsVue/admin/menuStaff/supportForEmployees.vue
@@ -1,15 +1,25 @@
 <template>
     <!-- Nội dung giao diện người dùng -->
-    suppord
+    <div class="ms-4 pt-2">
+        <p>Tên phân mềm : {{ nameSystem }}</p>
+        <p>Số phiên bản : {{ versionSystem }}</p>
+        <hr>
+        <span>Phần đóng góp ý kiến hoặc phản hồi</span>
+        <div class="content-support">
+            <input class="form-control input-title" type="text" placeholder="Tiêu đề" aria-label="default input ">
+        </div>
+    </div>
+
     <pre>
         .tên `cho phiên bản thử nghiệm``
         .content `hỗ trợ định dạng văn bản``
         . submit
     </pre>
-  </template>
+</template>
 
-  <script>
-  export default {
+<script>
+import ENV from '@/js/generalSetting/filterEnv.js'
+export default {
     name: 'supportForEmployees',
     components: {
     },
@@ -18,18 +28,20 @@
     directives: {
     },
     data() {
-      return {
-        // Dữ liệu của component
-      };
+        return {
+            nameSystem: ENV.SYSTEM_NAME,
+            versionSystem: ENV.VERSION
+            // Dữ liệu của component
+        };
     },
     created() {
-      // Logic khi component được khởi tạo
+        // Logic khi component được khởi tạo
     },
     mounted() {
-      // Logic sau khi component được gắn kết (render) vào DOM
+        // Logic sau khi component được gắn kết (render) vào DOM
     },
     computed() {
-      // được sử dụng để định nghĩa các thuộc tính tính toán
+        // được sử dụng để định nghĩa các thuộc tính tính toán
     },
     updated() {
 
@@ -38,11 +50,15 @@
 
     },
     methods: {
-      // Các phương thức xử lý sự kiện hoặc logic khác
+        // Các phương thức xử lý sự kiện hoặc logic khác
     },
-  };
-  </script>
+};
+</script>
 
-  <style>
-  /* CSS cho component */
-  </style>
+<style scoped>
+.input-title{
+    width: 80%;
+    margin: 1rem;
+}
+/* CSS cho component */
+</style>
