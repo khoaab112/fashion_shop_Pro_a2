aa836c5a587e24c95fd8c69442a0fde6f5141c0b
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2024-03-03 20:22:05 +0700

    backup add file backup _v4 _v5

---
+++ Note_Project/database/v4/data/administration_202403032021.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.administration (id,name,status,created_at,updated_at,code) VALUES
+	 (1,'SUPERADMIN',true,'2023-07-18 13:53:42.333383','2024-01-11 21:17:10.063413',NULL);
---
+++ Note_Project/database/v4/data/type_ticket_202403032021.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.type_ticket (id,name,note,status,created_at,updated_at,code) VALUES
+	 (3,'Phiếu thu',NULL,true,'2023-10-04 22:44:53.452815','2023-10-04 22:44:53.452815','IMPORT'),
+	 (4,'Phiếu xuất',NULL,true,'2023-10-04 22:44:53.464115','2023-10-04 22:44:53.464115','EXPORT');
